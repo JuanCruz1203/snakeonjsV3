303ca232df2af17f6f30e30688218fcb4badcf07
juancruzCT <juancruzctclases12@gmail.com>
2024-11-05 21:21:30 -0300

    nueva version con nuevas implementaciones

--- script.js
+++ script.js
@@ -19,6 +19,11 @@ var apple = {
     y: 320
 };
 
+var obstacles = []; // array para almacenar los obstáculos
+var obstacleSize = grid; // el tamaño de cada obstáculo (el mismo que el de la serpiente)
+var obstacleFactor = 2; // setea segun la dificultad (ya abajo se config el dropdown)
+var obstacleMaxCount = 10; // max de obstáculos que aparecerán
+var difficulty = "medium"; // valor predeterminado
 var score = 0;
 var bestScore = 0;
 var lastScoreIncrement = 0;
@@ -33,7 +38,7 @@ var currentSpeed = baseSpeed;
 var speedIncrement = 50;
 let isTextVisible = true; // Estado inicial de visibilidad
 let textBlinkCounter = 0; // Contador para controlar el titileo
-const blinkRate = 7; // Cada cuanto cambia la visibilidad
+const blinkRate = 7; 
 var gameState = 'menu'; // Estados: 'menu', 'playing', 'gameOver'
 
 var pauseImage = new Image();
@@ -45,12 +50,19 @@ gameOverImage.src = 'assets/gameover.png';
 var skullImage = new Image();
 skullImage.src = 'assets/calavera.png'; 
 
-const menuMusic = new Audio('assets/intromusic.mp3'); // musica del menú
+const menuMusic = new Audio('assets/intromusic.mp3'); // musica del main menu
 const gameOverMusic = new Audio('assets/gameovermusic.mp3'); // musica de Game Over
 const gameMusic = new Audio('assets/ingamesong.mp3'); // musica del juego
 
 var titleX = -200; 
 var menuY = 150;
+
+// esto es para actualizar el puntaje en el HTML
+function updateScoreDisplay() {
+    document.getElementById('best-score').textContent = 'Mejor: ' + bestScore + ' pts';
+    document.getElementById('current-score').textContent = 'Actual: ' + score + ' pts';
+}
+
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -67,7 +79,7 @@ function getRandomColor() {
 
 function drawScore() {
     context.fillStyle = 'white';
-    context.font = '8px "Press Start 2P", cursive'; // Usar la fuente retro
+    context.font = '8px "Press Start 2P", cursive';
     context.fillText('Best: ' + bestScore + ' pts', 10, 20);
     context.fillText('Now: ' + score + ' pts', 10, 40);
 }
@@ -79,7 +91,7 @@ function drawGameOver() {
     
     if (gameOverTransition >= 1 && gameOverTimer >= gameOverMessageDelay) {
         context.fillStyle = 'white';
-        context.font = '15px "Press Start 2P", cursive'; // fuente retro
+        context.font = '15px "Press Start 2P", cursive'; 
         const line1 = 'Perdiste!';
         context.fillText(line1, canvas.width / 2 - context.measureText(line1).width / 2, canvas.height / 2 + 30); // aca ajuste el valor de la transc.
 
@@ -87,7 +99,6 @@ function drawGameOver() {
         const line2 = 'x Apreta [R] para volver a jugar x';
         context.fillText(line2, canvas.width / 2 - context.measureText(line2).width / 2, canvas.height / 2 + 45); // menos espacio entre líneas
 
-        // Dibuja la imagen de la calavera
         context.drawImage(skullImage, canvas.width / 2 - 30, canvas.height / 2 - 100, 60, 60); // ajusta la position de la imagen
     }
 }
@@ -157,42 +168,139 @@ function drawSnake() {
             context.arc(cell.x + (3 * grid) / 4, cell.y + grid / 4, grid / 16, 0, Math.PI * 2);
             context.fill();
 
-            // Dibuja la lengua de la serpiente según la dirección
-            context.strokeStyle = 'red';
-            context.lineWidth = 2;
-            context.beginPath();
-            context.moveTo(cell.x + grid / 2, cell.y + grid);
-            if (snake.dy > 0) { // Abajo
-                context.lineTo(cell.x + grid / 2, cell.y + grid + 10);
-            } else if (snake.dy < 0) { // Arriba
-                context.lineTo(cell.x + grid / 2, cell.y - 10);
-            } else if (snake.dx > 0) { // Derecha
-                context.lineTo(cell.x + grid + 10, cell.y + grid / 2);
-            } else if (snake.dx < 0) { // Izquierda
-                context.lineTo(cell.x - 10, cell.y + grid / 2);
-            }
+            drawTongue(cell);
             context.stroke();
         }
     });
 }
+
+function drawTongue(headCell) {
+    const tongueLength = 10; 
+    const tongueWidth = 2; 
+    const tongueOffset = 5;
+
+    let startX = headCell.x + grid / 2;
+    let startY = headCell.y + grid / 2;
+
+    let endX = startX;
+    let endY = startY;
+
+    const oscillation = Math.sin(Date.now() / 100) * 5;
+
+    //direcciones en la que se mueve la lengua
+    if (snake.dx > 0) {
+        endX = startX + tongueLength;
+        endY = startY + oscillation; 
+    } else if (snake.dx < 0) {
+        endX = startX - tongueLength;
+        endY = startY + oscillation;
+    } else if (snake.dy > 0) {
+        endX = startX + oscillation; 
+        endY = startY + tongueLength;
+    } else if (snake.dy < 0) {
+        
+        endX = startX + oscillation; 
+        endY = startY - tongueLength;
+    }
+
+    // ahora lengua es una curva
+    context.beginPath();
+    context.moveTo(startX, startY);
+    context.quadraticCurveTo(
+        startX + (endX - startX) / 2, startY + (endY - startY) / 2, 
+        endX, endY 
+    );
+    context.lineWidth = tongueWidth;
+    context.strokeStyle = 'red'; 
+    context.stroke();
+    context.lineWidth = 1; 
+}
+
+
+
+function drawObstacles() {
+    context.fillStyle = 'yellow'; 
+    obstacles.forEach(function(obstacle) {
+        context.fillRect(obstacle.x, obstacle.y, obstacleSize, obstacleSize);
+    });
+}
+
 
 function drawTitle() {
     context.fillStyle = 'white';
     context.font = '15px "Press Start 2P", cursive';
     context.fillText('SNAKE GAME', titleX, 110);
 
-    // Dibuja la linea de puntos debajo del título
+    // Dibuja la linea de puntos debajo del titulo
     context.fillStyle = 'white';
     context.font = '10px "Press Start 2P", cursive'; 
     const lineY = 120; // Ajusta la pos vertical de la línea
     const lineLength = context.measureText('SNAKE GAME').width;
-    const dotSpacing = 7; // Espaciado entre puntos
+    const dotSpacing = 7; 
     let dots = '';
     for (let i = 0; i < lineLength / dotSpacing; i++) {
         dots += '.';
     }
     context.fillText(dots, titleX, lineY);
 }
+
+function setDifficulty() {
+    const difficultySelect = document.getElementById('difficulty');
+    difficulty = difficultySelect.value;
+
+    // Configuración para la dificultad
+    if (difficulty === "easy") {
+        obstacleFactor = 4; // + obstaculos, aparece con menor frecuencia
+    } else if (difficulty === "medium") {
+        obstacleFactor = 2; // preset normal
+    } else if (difficulty === "hard") {
+        obstacleFactor = 1; // - obstaculos, aparece con mayor frecuencia
+    }
+}
+
+
+function generateObstacles() {
+    obstacles = []; // hace un clean de obstaculos que existen
+    var maxObstacles = Math.floor(score / obstacleFactor); // cuantos obstaculos generar, de acuerdo al puntaje
+    maxObstacles = Math.min(maxObstacles, obstacleMaxCount); // limita el número de obstáculos
+
+    for (var i = 0; i < maxObstacles; i++) {
+        var obstacleX, obstacleY;
+        // se asegura de que los obstáculos no se generen en la serpiente ni en la manzana
+        do {
+            obstacleX = getRandomInt(0, canvas.width / grid) * grid;
+            obstacleY = getRandomInt(0, canvas.height / grid) * grid;
+        } while (isPositionOccupiedBySnake(obstacleX, obstacleY) || (obstacleX === apple.x && obstacleY === apple.y));
+
+        obstacles.push({ x: obstacleX, y: obstacleY });
+    }
+}
+
+function isPositionOccupiedBySnake(x, y) {
+    return snake.cells.some(function(cell) {
+        return cell.x === x && cell.y === y;
+    });
+}
+
+function checkCollisionWithObstacles() {
+    var head = snake.cells[0]; // La cabeza de la serpiente
+
+    for (var i = 0; i < obstacles.length; i++) {
+        var obstacle = obstacles[i];
+        if (head.x === obstacle.x && head.y === obstacle.y) {
+            gameOver = true; // Fin del juego si choca con un obst
+            gameOverTransition = 0;
+            gameOverTimer = 0;
+            gameOverMessageDisplayed = false;
+            gameOverMusic.play(); // Reproduce ost del Game Over
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
 
 function mostrarMenu() {
     context.fillStyle = 'black';
@@ -201,9 +309,8 @@ function mostrarMenu() {
     drawTitle(); 
 
     context.fillStyle = 'white';
-    context.font = '10px "Press Start 2P", cursive'; // Fuente retro
+    context.font = '10px "Press Start 2P", cursive'; 
     
-    // Dibuja el texto solo si está visible
     if (isTextVisible) {
         const text1 = 'INSERT COIN';
         const text2 = '[enter]';
@@ -211,6 +318,7 @@ function mostrarMenu() {
         context.fillText(text2, canvas.width / 2 - context.measureText(text2).width / 2, menuY + 30);
     }
 }
+
 
 function loop() {
     requestAnimationFrame(loop);
@@ -256,31 +364,34 @@ function loop() {
         }
 
         drawGameOver();
-        if (gameOverTransition >= 1 && !gameOverMusicPlayed) { // Reproduce música solo si no se ha reproducido
-            gameOverMusic.play(); // Reproduce la música de Game Over
-            gameOverMusicPlayed = true; // Marca que la música ya se ha reproducido
+        if (gameOverTransition >= 1 && !gameOverMusicPlayed) { 
+            gameOverMusic.play(); 
+            gameOverMusicPlayed = true; 
         }
         return;
     } else {
-        // Detenemos la música de juego aquí cuando se produce el Game Over
         gameMusic.pause();
-        gameMusic.currentTime = 0; // Reinicia la música de juego
-        gameOverMusic.pause(); // Asegúrate de que la música de Game Over se pause
-        gameOverMusic.currentTime = 0; // Reinicia la música de Game Over
+        gameMusic.currentTime = 0; 
+        gameOverMusic.pause(); 
+        gameOverMusic.currentTime = 0;
     }
 
+    // Generar obstaculos cada vez que la puntuación sea multiplo de 5
     if (score > lastScoreIncrement && score % 5 === 0) {
         lastScoreIncrement = score;
         currentSpeed += speedIncrement;
         snake.color = getRandomColor();
+        generateObstacles(); // Generar nuevos obstáculos
     }
 
     context.fillStyle = 'black';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
+    // Mover la serpiente
     snake.x += snake.dx;
     snake.y += snake.dy;
 
+    // Revisar límites de la pantalla y mover la serpiente de forma "envolvente"
     if (snake.x < 0) {
         snake.x = canvas.width - grid;
     } else if (snake.x >= canvas.width) {
@@ -293,15 +404,19 @@ function loop() {
         snake.y = 0;
     }
 
+    // Agregar la nueva cabeza de la serpiente
     snake.cells.unshift({ x: snake.x, y: snake.y });
 
+    // Si la serpiente ha alcanzado el tamaño máximo, eliminar ultimo segmento
     if (snake.cells.length > snake.maxCells) {
         snake.cells.pop();
     }
 
+    
     drawApple();
     drawSnake();
 
+    // Verificar si la serpiente ha comido la manzana
     snake.cells.forEach(function(cell, index) {
         if (cell.x === apple.x && cell.y === apple.y) {
             snake.maxCells++;
@@ -313,29 +428,49 @@ function loop() {
             apple.y = getRandomInt(0, 25) * grid;
         }
 
+        // Verificar si la serpiente se ha chocado asi misma
         for (var i = index + 1; i < snake.cells.length; i++) {
             if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
                 gameOver = true;
                 gameOverTransition = 0;
                 gameOverTimer = 0;
                 gameOverMessageDisplayed = false;
-                
-                // Detenemos la música del juego al perder
+
+                // Detenemos musica del juego al perder
                 gameMusic.pause();
-                gameMusic.currentTime = 0; // Reinicia la música de juego
+                gameMusic.currentTime = 0; // Reinicia  musica juego
             }
         }
     });
 
-    drawScore();
+    // Verificar si la serpiente choca con obstaculos
+    if (checkCollisionWithObstacles()) {
+        gameOver = true;
+        gameOverTransition = 0;
+        gameOverTimer = 0;
+        gameOverMessageDisplayed = false;
+        
+        
+        gameMusic.pause();
+        gameMusic.currentTime = 0; 
+    }
+
+    // Dibujar los obstáculos
+    drawObstacles();
+
+    updateScoreDisplay();
+
+    
 }
 
-var gameOverMusicPlayed = false; // Nueva variable para controlar la reproducción
+
+var gameOverMusicPlayed = false; 
 
 function restartGame() {
     snake.x = 160;
     snake.y = 160;
     snake.cells = [];
+    obstacles = []; 
     snake.maxCells = 4;
     snake.dx = grid;
     snake.dy = 0;
@@ -355,7 +490,7 @@ function restartGame() {
     snake.color = 'green';
     gameState = 'playing'; // Cambia el estado a jugar
 
-    // Detiene la música de Game Over
+   
     gameOverMusic.pause();
     gameOverMusic.currentTime = 0;
 }
@@ -363,7 +498,7 @@ function restartGame() {
 document.addEventListener('keydown', function(e) {
     if (gameOver) {
         if (e.which === 82) { // R para reiniciar
-            gameMusic.pause(); // Detiene la música del juego
+            gameMusic.pause(); // detiene la música del juego
             restartGame();
         }
         return;
@@ -372,12 +507,12 @@ document.addEventListener('keydown', function(e) {
     if (gamePaused) {
         if (e.which === 67) { // C para continuar
             gamePaused = false;
-            gameMusic.play(); // Reanuda la música del juego
-        } else if (e.which === 82) { // R para reiniciar
-            gameMusic.pause(); // Detiene la música del juego
+            gameMusic.play(); 
+        } else if (e.which === 82) {
+            gameMusic.pause(); // 
             restartGame();
-        } else if (e.which === 81) { // Q para salir
-            gameMusic.pause(); // Detiene la música del juego
+        } else if (e.which === 81) { 
+            gameMusic.pause(); // pausa la musica
             restartGame();
             gameState = 'menu';
             titleX = -200;
@@ -394,6 +529,7 @@ document.addEventListener('keydown', function(e) {
 
     if (gameState === 'menu') {
         if (e.which === 13) { // ENTER para iniciar
+            setDifficulty(); // Configura la dificultad
             gameState = 'playing';
             restartGame();
         }
